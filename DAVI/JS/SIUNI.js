e131367355aaefcb62bcfb2617d30b4b070edf3f
Davi-Donato-PUC <davidonato.puc@gmail.com>
2024-10-25 22:08:16 -0300

    BarraLateralSiuni

--- DAVI/JS/SIUNI.js
+++ DAVI/JS/SIUNI.js
@@ -1,8 +1,18 @@
-function LoadPageContent(Page) {
+
+let botaoAnterior = null;
+
+function LoadPageContent(Page, event) {
     fetch(Page)
         .then(response => response.text())
         .then(data => { document.getElementById("Conteudo").innerHTML = data } )
         .catch(error => console.log("Não foi possivel carregar: ", error ) )
+
+    let botaoAtual = event.currentTarget
+    if (botaoAnterior) { botaoAnterior.style.backgroundColor = ''; }
+    botaoAtual.style.backgroundColor = 'rgba(255, 255, 255, 0.1)';
+    botaoAnterior = botaoAtual;
+
+
     }
 
 
