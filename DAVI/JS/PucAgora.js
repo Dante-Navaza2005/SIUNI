bc767347beb6c36600ffd1bb8b7aa8b50d596a3b
Davi-Donato-PUC <davidonato.puc@gmail.com>
2024-11-03 14:01:27 -0300

    PucAgora

---
+++ DAVI/JS/PucAgora.js
@@ -0,0 +1,21 @@
+
+
+function AdicionarNoticia() {
+    let Noticia = document.createElement('div')
+    fetch('../HTML/Noticia.html')
+    .then(resposta => resposta.text())
+    .then(html => {Noticia.innerHTML = html; Noticia.classList.add("Noticia")})
+    .catch(error => { console.error('Erro erro', error); });
+    
+    const feed = document.getElementById('AreaDeConteudo')
+    feed.appendChild(Noticia)
+    }
+
+
+
+
+
+
+
+
+
